790449ed7fa89f31642ff61fbb1b4bff070b8f5a
Ben Taylor <taylor.ben89@gmail.com>
2017-04-03 13:38:06 +0000

    added 4 tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,25 @@
 require 'fizzbuzz'
 
-describe 'fizzbuzz' do
-    it 'returns "fizz" when passed 3' do
+describe 'fizz' do
+    it 'returns "fizz" when passed multiple of 3' do
         expect(fizzbuzz(3)).to eq 'fizz'
+    end
+end
+
+describe 'buzz' do
+    it 'returns "buzz" when passed multiple of 5' do
+        expect(fizzbuzz(5)).to eq 'buzz'
+    end
+end
+
+describe 'fizzbuzz' do
+    it 'returns "fizzbuzz" when passed a multiple of 3 and 5' do
+        expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    end
+end
+
+describe 'other_numbers' do
+    it 'returns number when passes a number which is not a multiple of 3 or 5' do
+        expect(fizzbuzz(8)).to eq '8'
     end
 end
